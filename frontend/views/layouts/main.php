156d0f0b8d6d8063b05d0fc1fdfdbeafd45426f6
Tumanggor <mendienta990@gmail.com>
2024-11-26 11:02:15 +0700

    Auto-commit: Updates made on Tue Nov 26 11:02:15 AM WIB 2024

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -40,24 +40,11 @@ AppAsset::register($this);
         ['label' => 'List Dokter', 'url' => ['/doctors/index']], // Tambahkan menu Produk
         ['label' => 'Konsultasi', 'url' => ['/consultation/index']], // Tambahkan menu Produk
     ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-    }
 
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav me-auto mb-2 mb-md-0'],
         'items' => $menuItems,
     ]);
-    if (Yii::$app->user->isGuest) {
-        echo Html::tag('div',Html::a('Login',['/site/login'],['class' => ['btn btn-link login text-decoration-none']]),['class' => ['d-flex']]);
-    } else {
-        echo Html::beginForm(['/site/logout'], 'post', ['class' => 'd-flex'])
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout text-decoration-none']
-            )
-            . Html::endForm();
-    }
     NavBar::end();
     ?>
 </header>
